c000d06d589cab908e6d5891ce4f29492457513e
studio4app <contact@studio4app.com>
2021-10-06 01:30:35 +0200

    CommitAll added new Console.WriteLine

--- ConsoleCSharp/Program.cs
+++ ConsoleCSharp/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleCSharp
             for(int i=0; i<10; i++)
             {
                 Console.WriteLine("Hello in iteration" + i);
+                Console.WriteLine("ChangesFromLocalMachine" + i*2);
             }
             Console.ReadKey();
         }
