b3f5e36615fcf0166a9d66381b953f1d0fbbdd74
sgrabinski <sgrabinski@parasoft.com>
2021-10-07 18:01:53 +0200

    Console.WriteLine("Hello Master Branch in iteration" + i);

--- ConsoleCSharp/Program.cs
+++ ConsoleCSharp/Program.cs
@@ -12,8 +12,7 @@ namespace ConsoleCSharp
         {
             for(int i=0; i<10; i++)
             {
-                Console.WriteLine("Hello in iteration" + i);
-                Console.WriteLine("ChangesFromLocalMachine" + i*2);
+                Console.WriteLine("Hello Master Branch in iteration" + i);
             }
             Console.ReadKey();
         }
