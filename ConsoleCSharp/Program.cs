3747386ac9cdb603f0d82a4828cddf1f15b71e92
sgrabinski <sgrabinski@parasoft.com>
2021-10-08 15:35:36 +0200

    changes to test prepared new Branch2. If you accepted I will merge into newBranch.

--- ConsoleCSharp/Program.cs
+++ ConsoleCSharp/Program.cs
@@ -6,19 +6,9 @@ namespace ConsoleCSharp
     {
         static void Main(string[] args)
         {
-            WriteMsgUseCondition(); // function checking condition what should be written
+            WriteMessage writeMessage = new WriteMessage();
+            writeMessage.Bool(true);
             Console.ReadKey();
         }
-        static void WriteMsgUseCondition(bool b = false) // by default it's false argument
-        {
-            if (b == true)
-            {
-                Console.WriteLine("NewBranch with condition: " + b);
-            }
-            else
-            {
-                Console.WriteLine("NewBranch with condition: " + b);
-            }
-        }
-    }
+          }
 }
