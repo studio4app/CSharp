488b648f115b38a9b4f58f6c06835fb483bbecec
studio4app <contact@studio4app.com>
2021-10-14 21:25:23 +0200

    add thread into code

--- ConsoleCSharp/Program.cs
+++ ConsoleCSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleCSharp
@@ -10,24 +11,30 @@ namespace ConsoleCSharp
     {
         static void Main(string[] args)
         {
-            ShowHello();
-            ShowHi();
+            Thread thread1 = new Thread(()=>ShowHello("Thread #1"));
+            thread1.Start();
+            Thread thread2 = new Thread(() => ShowHi("Thread #2"));
+            thread2.Start();
 
             Console.ReadKey();
         }
-        static void ShowHello()
+        static void ShowHello(string name)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Hello Master Branch in iteration" + i);
+                Console.WriteLine("{0} Hello Master Branch in iteration " + i, name);
+                Thread.Sleep(1000);
             }
+            Console.WriteLine("{0} ends", name);
         }
-        static void ShowHi()
+        static void ShowHi(string name)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Hi Master Branch in iteration" + i);
+                Console.WriteLine("{0} Hi Master Branch in iteration " + i, name);
+                Thread.Sleep(1000);
             }
+            Console.WriteLine("{0} ends", name);
         }
     }
 }
